4db80d0fd3d9157a49052588bfd7ee9718afc1de
NoRuTnT <moonabcd@naver.com>
2025-09-22 20:05:19 +0900

    feat: currentscreen으로 화면전환

--- composeApp/src/jsMain/kotlin/org/whatsong/project/App.kt
+++ composeApp/src/jsMain/kotlin/org/whatsong/project/App.kt
@@ -2,17 +2,52 @@ package org.whatsong.project
 
 
 import androidx.compose.runtime.*
+import org.whatsong.project.api.TokenStorage
+import org.whatsong.project.api.createTokenStorage
+import org.whatsong.project.navigation.Screen
+import org.whatsong.project.page.LobbyScreen
+import org.whatsong.project.page.LoginScreen
+import org.whatsong.project.viewmodels.LobbyViewModel
+import org.whatsong.project.viewmodels.LoginViewModel
+import org.whatsong.project.viewmodels.UserViewModel
 
 @Composable
-fun App() {
-    var currentPage by remember { mutableStateOf("login") }
-    val loginState = remember { LoginState() }
+fun App(
+    loginViewModel: LoginViewModel,
+    userViewModel: UserViewModel,
+    lobbyViewModel: LobbyViewModel,
+    tokenStorage: TokenStorage = createTokenStorage()
 
-    when (currentPage) {
-        "login" -> LoginScreenWeb(
-            loginState = loginState,
-            onLoginSuccess = { currentPage = "lobby" }
+) {
+    var currentScreen by remember { mutableStateOf<Screen>(Screen.Login) }
+
+    when (val screen = currentScreen) {
+        // 로그인 화면
+        Screen.Login -> {
+            val state by loginViewModel.loginState.collectAsState()
+            if (state.isLoggedIn) {
+                val token = tokenStorage.getAccessToken() ?: ""
+                userViewModel.loadUserInfo(token)
+                currentScreen = Screen.Lobby
+            }
+
+            LoginScreen(
+                loginViewModel = loginViewModel
+            )
+        }
+
+        // 로비 화면
+        Screen.Lobby -> LobbyScreen(
+            lobbyViewModel = lobbyViewModel,
+            userViewModel = userViewModel,
+            onNavigateToGame = { roomNo, body ->
+                currentScreen = Screen.Lobby //todo 게임방화면만들면 바꾸기
+            }
         )
-        "lobby" -> LobbyScreen()
+
+//        is Screen.Game -> GameScreen(
+//            roomNo = screen.roomNo,
+//            onNavigateBack = { currentScreen = Screen.Lobby }
+//        )
     }
 }
