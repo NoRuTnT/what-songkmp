37f58b2e39817a6d2c0765332363c8c13ff86fd4
NoRuTnT <moonabcd@naver.com>
2025-09-22 20:04:59 +0900

    feat: main에 시작시 주입시킬것들 추가

--- composeApp/src/jsMain/kotlin/org/whatsong/project/main.kt
+++ composeApp/src/jsMain/kotlin/org/whatsong/project/main.kt
@@ -2,9 +2,37 @@ package org.whatsong.project
 
 
 import org.jetbrains.compose.web.renderComposable
+import org.whatsong.project.api.createTokenStorage
+import org.whatsong.project.api.repository.AuthRepository
+import org.whatsong.project.api.repository.LobbyRepository
+import org.whatsong.project.api.repository.UserRepository
+import org.whatsong.project.api.whatsongApis
+import org.whatsong.project.viewmodels.LobbyViewModel
+import org.whatsong.project.viewmodels.LoginViewModel
+import org.whatsong.project.viewmodels.UserViewModel
 
 fun main() {
+    val WHATSONG_BASE_URL = "https://"
+
+
+    val tokenStorage = createTokenStorage()
+    val apiClient = whatsongApis(WHATSONG_BASE_URL,tokenStorage)
+    val userRepository = UserRepository(apiClient)
+    val authRepository = AuthRepository(apiClient)
+    val lobbyRepository = LobbyRepository(apiClient)
+
+
+    val loginViewModel = LoginViewModel(authRepository, tokenStorage)
+    val userViewModel = UserViewModel(userRepository)
+    val lobbyViewModel = LobbyViewModel(lobbyRepository)
+
+
     renderComposable(rootElementId = "root") {
-        App()
+        App(
+            loginViewModel = loginViewModel,
+            userViewModel = userViewModel,
+            lobbyViewModel = lobbyViewModel,
+            tokenStorage = tokenStorage
+        )
     }
 }
